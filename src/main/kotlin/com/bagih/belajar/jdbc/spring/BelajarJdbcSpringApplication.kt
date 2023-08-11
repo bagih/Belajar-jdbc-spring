c6a06b4b12beb6ff60931b200183f67d535944e3
Bagi Hartawan <hartawanbagie@gmail.com>
2023-08-11 23:17:47 +0800

    lesson 1 learned: setting up the H2 Database

---
+++ src/main/kotlin/com/bagih/belajar/jdbc/spring/BelajarJdbcSpringApplication.kt
@@ -0,0 +1,11 @@
+package com.bagih.belajar.jdbc.spring
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BelajarJdbcSpringApplication
+
+fun main(args: Array<String>) {
+	runApplication<BelajarJdbcSpringApplication>(*args)
+}
---
+++ src/test/kotlin/com/bagih/belajar/jdbc/spring/BelajarJdbcSpringApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.bagih.belajar.jdbc.spring
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BelajarJdbcSpringApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
