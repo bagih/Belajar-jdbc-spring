bd82c372d2b6dbe3658c36efd0a5b3b7fe76bfc8
Bagi Hartawan <hartawanbagie@gmail.com>
2023-08-15 10:53:11 +0800

    fixing select data from db

--- src/main/kotlin/com/bagih/belajar/jdbc/spring/BelajarJdbcSpringApplication.kt
+++ src/main/kotlin/com/bagih/belajar/jdbc/spring/BelajarJdbcSpringApplication.kt
@@ -1,6 +1,7 @@
 package com.bagih.belajar.jdbc.spring
 
 import com.bagih.belajar.jdbc.spring.data.PlayerDao
+import com.bagih.belajar.jdbc.spring.model.Player
 import lombok.extern.slf4j.Slf4j
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
@@ -8,6 +9,10 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import java.sql.Timestamp
+import java.time.Instant
+import java.time.LocalDateTime
+import java.util.Date
 import kotlin.jvm.Throws
 
 @SpringBootApplication
@@ -27,6 +32,12 @@ class BelajarJdbcSpringApplication(
 //			e.printStackTrace()
 //		}
 //		logger.info("get all players: ${playerDao.getAllPlayers()}")
+//		println("commandline runner is called")
+//		println("get player: ${playerDao.getAllPlayers()}")
+		logger.info("get all players: ${playerDao.getAllPlayers()}")
+		logger.info("get player by id: ${playerDao.getPlayerById(2)}")
+		playerDao.insertPlayer(Player(id = 4, name = "simbur", nationality = "JAWA", birthDate = Date(System.currentTimeMillis()), titles = 20))
+		logger.info("players after insert: ${playerDao.getAllPlayers()}")
 	}
 }
 
--- src/main/kotlin/com/bagih/belajar/jdbc/spring/data/PlayerDao.kt
+++ src/main/kotlin/com/bagih/belajar/jdbc/spring/data/PlayerDao.kt
@@ -4,7 +4,10 @@ import com.bagih.belajar.jdbc.spring.model.Player
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.jdbc.core.BeanPropertyRowMapper
 import org.springframework.jdbc.core.JdbcTemplate
+import org.springframework.jdbc.core.RowMapper
 import org.springframework.stereotype.Repository
+import java.sql.ResultSet
+import java.sql.Timestamp
 import java.util.Objects
 import kotlin.reflect.jvm.internal.impl.load.kotlin.JvmType
 
@@ -12,19 +15,31 @@ import kotlin.reflect.jvm.internal.impl.load.kotlin.JvmType
 class PlayerDao(
     @Autowired
     val jdbcTemplate: JdbcTemplate
-){
-    fun getAllPlayers(): List<Player>{
-        val sql: String = "SELECT * FROM PLAYER"
-        return jdbcTemplate.query(sql, BeanPropertyRowMapper<Player>(Player::class.java))
+) {
+
+    val rowMapper: RowMapper<Player> = RowMapper<Player> { rs: ResultSet, rowNum: Int ->
+        Player(
+            rs.getInt("id"),
+            rs.getString("name"),
+            rs.getString("nationality"),
+            rs.getDate("birth_date"),
+            rs.getInt("titles")
+        )
+
     }
 
-    fun getPlayerById(id: Int): Player?{
+    fun getAllPlayers(): List<Player> {
+        val sql: String = "SELECT * FROM Player"
+        return jdbcTemplate.query(sql, rowMapper)
+    }
+
+    fun getPlayerById(id: Int): Player? {
         val sql: String = "SELECT * FROM PLAYER WHERE ID = ?"
-        return jdbcTemplate.queryForObject(sql, BeanPropertyRowMapper<Player>(Player::class.java))
+        return jdbcTemplate.queryForObject(sql, rowMapper, id)
     }
 
-    fun insertPlayer(player: Player){
+    fun insertPlayer(player: Player) {
         val sql = "INSERT INTO PLAYER (ID, Name, Nationality, Birth_date, Titles) VALUES (?, ?, ?, ?, ?)"
-        jdbcTemplate.update(sql, player.id, player.name, player.nationality, player.birthDate, player.titles)
+        jdbcTemplate.update(sql, arrayOf(player.id, player.name, player.nationality, Timestamp(player.birthDate.time), player.titles))
     }
 }
